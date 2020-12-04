e0526501713642b073526413bdf5ec1fa2a0ebc6
Drew <andrew.althage@gmail.com>
2020-12-03 17:23:30 -0700

    initial setup

---
+++ routes/api.php
@@ -0,0 +1,71 @@
+<?php
+
+use Dingo\Api\Routing\Router;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*
+ * Welcome route - link to any public API documentation here
+ */
+Route::get('/', function () {
+    echo 'Welcome to our API';
+});
+
+/** @var \Dingo\Api\Routing\Router $api */
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', ['middleware' => ['api']], function (Router $api) {
+    /*
+     * Authentication
+     */
+    $api->group(['prefix' => 'auth'], function (Router $api) {
+        $api->group(['prefix' => 'jwt'], function (Router $api) {
+            $api->get('/token', 'App\Http\Controllers\Auth\AuthController@token');
+        });
+    });
+
+    /*
+     * Authenticated routes
+     */
+    $api->group(['middleware' => ['api.auth']], function (Router $api) {
+        /*
+         * Authentication
+         */
+        $api->group(['prefix' => 'auth'], function (Router $api) {
+            $api->group(['prefix' => 'jwt'], function (Router $api) {
+                $api->get('/refresh', 'App\Http\Controllers\Auth\AuthController@refresh');
+                $api->delete('/token', 'App\Http\Controllers\Auth\AuthController@logout');
+            });
+
+            $api->get('/me', 'App\Http\Controllers\Auth\AuthController@getUser');
+        });
+
+        /*
+         * Users
+         */
+        $api->group(['prefix' => 'users', 'middleware' => 'check_role:admin'], function (Router $api) {
+            $api->get('/', 'App\Http\Controllers\UserController@getAll');
+            $api->get('/{uuid}', 'App\Http\Controllers\UserController@get');
+            $api->post('/', 'App\Http\Controllers\UserController@post');
+            $api->put('/{uuid}', 'App\Http\Controllers\UserController@put');
+            $api->patch('/{uuid}', 'App\Http\Controllers\UserController@patch');
+            $api->delete('/{uuid}', 'App\Http\Controllers\UserController@delete');
+        });
+
+        /*
+         * Roles
+         */
+        $api->group(['prefix' => 'roles'], function (Router $api) {
+            $api->get('/', 'App\Http\Controllers\RoleController@getAll');
+        });
+    });
+});
